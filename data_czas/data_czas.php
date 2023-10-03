96a2fadb53ac60a316396bb09196b8129e05636b
arthulus <arcziwcislo@gmail.com>
2023-10-03 14:40:29 +0200

    Data i czas

---
+++ data_czas/data_czas.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Dzisiaj jest " . date("Y/m/d") . "<br>";
+    echo "Dzisiaj jest " . date("d.m.Y") . "<br>";
+    echo "Dzisiaj jest " . date("d-m-Y") . "<br>";
+    echo "Dzisiaj jest " . date("l") . "<br>";
+?>
